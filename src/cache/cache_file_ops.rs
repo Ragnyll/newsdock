26b91b755976f0f75d060418c855d95ebb282146
ragnyll <ragnyll@gallowzhumour.dev>
2022-01-02 16:17:43 -0600

    add cli for downloader workflow

--- src/cache/cache_file_ops.rs
+++ src/cache/cache_file_ops.rs
@@ -2,9 +2,9 @@ use std::path::{Path, PathBuf};
 use std::fs;
 
 /// returns whether or not the cache contains a file with the specified basename
-pub fn check_cache(f_basename: &str, cache_location: Option<&str>) -> bool {
+pub fn check_cache(f_basename: &str, cache_location: &str) -> bool {
     let home_dir: PathBuf = dirs::home_dir().expect("Unable to find home dir while checking cache");
-    let path = Path::new(&home_dir).join(cache_location.unwrap_or(super::DEFAULT_CACHE_LOCATION));
+    let path = Path::new(&home_dir).join(cache_location);
 
     for file in fs::read_dir(&path).unwrap() {
         if file.unwrap().path().file_stem().unwrap() == f_basename {
