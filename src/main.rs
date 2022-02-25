e49b41981c082bde980c1b7b5d71e36514481f6a
ragnyll <ragnyll@gallowzhumour.dev>
2022-02-24 23:54:42 -0600

    anyhow for bin

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,46 @@
+use anyhow::Result;
 use newsdock::cache;
 use newsdock::conf;
 use newsdock::conf::CmdType;
-use newsdock::db::QueryManager;
+use newsdock::db::{DbError, QueryManager};
 use newsdock::fs;
 use newsdock::newsboat_utils::bin_utils;
 use newsdock::opener;
-use std::process;
 
-fn main() {
+fn main() -> Result<()>{
     let conf = conf::build_conf().unwrap();
 
     // TODO: replace unwraps at end of decls with anyhow
-    let db_location = fs::get_file_location_or_abort(&conf.cache_db_location).unwrap();
-    let newsboat_config_location = fs::get_file_location_or_abort(&conf.newsboat_config_location).unwrap();
-    let cache_dir = fs::get_dir_or_create(&conf.cache_dir).unwrap();
+    let db_location = fs::get_file_location_or_abort(&conf.cache_db_location)?;
+    let newsboat_config_location = fs::get_file_location_or_abort(&conf.newsboat_config_location)?;
+    let cache_dir = fs::get_dir_or_create(&conf.cache_dir)?;
 
-    let query_manager = match QueryManager::new(&db_location) {
-        Ok(qm) => {
-            log::info!("Connection established by query manager");
-            qm
-        }
-        Err(e) => {
-            log::error!("Failed to connect to DB using query manager: {e}");
-            process::exit(exitcode::DATAERR);
-        }
-    };
+    let query_manager = QueryManager::new(&db_location)?;
 
     match conf.cmd_type {
         CmdType::Dl => {
             let newsboat_urls_location =
-                fs::get_file_location_or_abort(&conf.newsboat_urls_location.unwrap()).unwrap();
-            download(conf.skip_refresh.unwrap(), &db_location, &newsboat_urls_location, &newsboat_config_location, &cache_dir, conf.yt_dlp_attempts.unwrap(), query_manager);
+                fs::get_file_location_or_abort(&conf.newsboat_urls_location.unwrap())?;
+            download(conf.skip_refresh.unwrap(), &db_location, &newsboat_urls_location, &newsboat_config_location, &cache_dir, conf.yt_dlp_attempts.unwrap(), query_manager)?;
         }
         CmdType::Open => {
-            open(&conf.open_url.unwrap(), &"rifle", &cache_dir, query_manager);
+            open(&conf.open_url.unwrap(), &"rifle", &cache_dir, query_manager)?;
         }
         CmdType::Clean => {
             eprintln!("Cache Clean not yet implemented")
         }
     }
+
+    Ok(())
 }
 
-fn open(url: &str, opener_bin: &str, cache_dir: &str, query_manager: QueryManager) {
-    let _ = opener::open(
+fn open(url: &str, opener_bin: &str, cache_dir: &str, query_manager: QueryManager) -> Result <(), opener::OpenerError>{
+    opener::open(
         url,
         Some(String::from(opener_bin)),
         Some(String::from(cache_dir)),
         query_manager,
-    );
+    )
 }
 
 fn download(
@@ -57,7 +50,7 @@ fn download(
     newsboat_config_location: &str,
     cache_dir: &str,
     yt_dlp_attempts: u32,
-    query_manager: QueryManager) {
+    query_manager: QueryManager) -> Result<(), DbError> {
     if !skip_refresh {
         match bin_utils::reload_feed_items(
             &db_location,
@@ -69,13 +62,7 @@ fn download(
         };
     }
 
-    let item_urls = match query_manager.get_all_cacheable_feed_items() {
-        Ok(urls) => urls,
-        Err(e) => {
-            log::error!("Failed to retrieve urls to download {e}");
-            process::exit(exitcode::DATAERR);
-        }
-    };
+    let item_urls = query_manager.get_all_cacheable_feed_items()?;
 
     for item in item_urls {
         match cache::downloader::poll_cache(&item, Some(String::from(cache_dir)), yt_dlp_attempts) {
@@ -85,4 +72,6 @@ fn download(
             }
         }
     }
+
+    Ok(())
 }
