5333425be8f626170e2a037dfc5214c130d45393
ragnyll <ragnyll@gallowzhumour.dev>
2022-02-27 22:24:00 -0600

    move cli options to commands

--- src/conf/cli.rs
+++ src/conf/cli.rs
@@ -4,25 +4,9 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[clap(name = "newsdock")]
 pub struct Cli {
-    #[clap(long, default_value = "error")]
-    pub log_level: String,
-
     #[clap(subcommand)]
     pub command: Commands,
 
-    /// An optional location for the default cache directory
-    #[clap(long, default_value = ".cache/newsdock/")]
-    pub cache_dir: String,
-
-    /// An optional override for the location where the newsboat urls file is stored relative to
-    /// the home dir
-    #[clap(long, default_value = ".config/newsboat/config")]
-    pub newsboat_config_location: String,
-
-    /// An opitonal override for the location where newsboats db is stored relative to the home_dir
-    /// defaults to "/.local/share/newsboat/cache.db"
-    #[clap(long, default_value = ".local/share/newsboat/cache.db")]
-    pub cache_db_location: String,
 }
 
 #[derive(Subcommand, Debug)]
@@ -32,6 +16,25 @@ pub enum Commands {
     Open {
         /// The url to download
         url: String,
+
+        /// An optional location for the default cache directory
+        #[clap(long, default_value = ".cache/newsdock/")]
+        cache_dir: String,
+
+        /// An optional override for the location where the newsboat urls file is stored relative to
+        /// the home dir
+        #[clap(long, default_value = ".config/newsboat/config")]
+        newsboat_config_location: String,
+
+        /// An opitonal override for the location where newsboats db is stored relative to the home_dir
+        /// defaults to "/.local/share/newsboat/cache.db"
+        #[clap(long, default_value = ".local/share/newsboat/cache.db")]
+        cache_db_location: String,
+
+        /// log level allowed values are info, error, trace
+        #[clap(long, default_value = "error")]
+        log_level: String,
+
     },
 
     /// Updates the cache with all the items to download
@@ -48,10 +51,80 @@ pub enum Commands {
         /// The amount of times to retry downloads from youtube
         #[clap(long, default_value_t = 20)]
         yt_dlp_attempts: u32,
+
+        /// An optional location for the default cache directory
+        #[clap(long, default_value = ".cache/newsdock/")]
+        cache_dir: String,
+
+        /// An optional override for the location where the newsboat urls file is stored relative to
+        /// the home dir
+        #[clap(long, default_value = ".config/newsboat/config")]
+        newsboat_config_location: String,
+
+        /// An opitonal override for the location where newsboats db is stored relative to the home_dir
+        /// defaults to "/.local/share/newsboat/cache.db"
+        #[clap(long, default_value = ".local/share/newsboat/cache.db")]
+        cache_db_location: String,
+
+        /// log level allowed values are info, error, trace
+        #[clap(long, default_value = "error")]
+        log_level: String,
     },
 
     /// Cleans the cache following the an oldest first eviction policy
-    Clean {},
+    Clean {
+        /// An optional location for the default cache directory
+        #[clap(long, default_value = ".cache/newsdock/")]
+        cache_dir: String,
+
+        /// An optional override for the location where the newsboat urls file is stored relative to
+        /// the home dir
+        #[clap(long, default_value = ".config/newsboat/config")]
+        newsboat_config_location: String,
+
+        /// An opitonal override for the location where newsboats db is stored relative to the home_dir
+        /// defaults to "/.local/share/newsboat/cache.db"
+        #[clap(long, default_value = ".local/share/newsboat/cache.db")]
+        cache_db_location: String,
+
+        /// log level allowed values are info, error, trace
+        #[clap(long, default_value = "error")]
+        log_level: String,
+    },
+
+    /// Downloads new items to the cache then evicts old items
+    Update {
+        /// An optional override for the location where the newsboat urls file is stored relative to
+        /// the home dir
+        #[clap(long, default_value = ".config/newsboat/urls")]
+        newsboat_urls_location: String,
+
+        /// skips the refresh on the newsboatdb
+        #[clap(long)]
+        skip_refresh: bool,
+
+        /// The amount of times to retry downloads from youtube
+        #[clap(long, default_value_t = 20)]
+        yt_dlp_attempts: u32,
+
+        /// An optional location for the default cache directory
+        #[clap(long, default_value = ".cache/newsdock/")]
+        cache_dir: String,
+
+        /// An optional override for the location where the newsboat urls file is stored relative to
+        /// the home dir
+        #[clap(long, default_value = ".config/newsboat/config")]
+        newsboat_config_location: String,
+
+        /// An opitonal override for the location where newsboats db is stored relative to the home_dir
+        /// defaults to "/.local/share/newsboat/cache.db"
+        #[clap(long, default_value = ".local/share/newsboat/cache.db")]
+        cache_db_location: String,
+
+        /// log level allowed values are info, error, trace
+        #[clap(long, default_value = "error")]
+        log_level: String,
+    },
 }
 
 pub fn get_cli_args() -> Cli {
--- src/conf/mod.rs
+++ src/conf/mod.rs
@@ -1,6 +1,7 @@
 mod cli;
 
 use log::LevelFilter;
+use simple_logger::SimpleLogger;
 use std::convert::From;
 use std::str::FromStr;
 use thiserror::Error;
@@ -8,16 +9,23 @@ use thiserror::Error;
 /// builds a conf from the aggregate of configuration file arguments and cli arguments
 pub fn build_conf() -> Result<Conf, ConfError> {
     let cli_args = cli::get_cli_args();
-    LevelFilter::from_str(&cli_args.log_level)?;
 
-    Ok(Conf::from(cli_args))
+    merge_confs(None, Conf::from(cli_args))
 }
 
 // TODO
 /// Merges two confs into a single one with the second overriding the first where applicable
 #[allow(dead_code)]
-#[allow(unused_variables)]
-fn merge_confs(conf_1: Conf, conf_2: Conf) {}
+fn merge_confs(_conf_1: Option<Conf>, conf_2: Conf) -> Result<Conf, ConfError> {
+    let level = LevelFilter::from_str(&conf_2.log_level)?;
+    SimpleLogger::new()
+        .with_local_timestamps()
+        .with_level(level)
+        .init()
+        .unwrap();
+
+    Ok(conf_2)
+}
 
 pub enum CmdType {
     Dl,
@@ -46,39 +54,74 @@ impl From<cli::Cli> for Conf {
                 newsboat_urls_location,
                 skip_refresh,
                 yt_dlp_attempts,
+                cache_dir,
+                newsboat_config_location,
+                cache_db_location,
+                log_level,
             } => Self {
                 cmd_type: CmdType::Dl,
-                log_level: cli.log_level,
-                cache_dir: cli.cache_dir,
-                newsboat_config_location: cli.newsboat_config_location,
-                cache_db_location: cli.cache_db_location,
+                log_level: log_level,
+                cache_dir: cache_dir,
+                newsboat_config_location: newsboat_config_location,
+                cache_db_location: cache_db_location,
                 open_url: None,
                 newsboat_urls_location: Some(newsboat_urls_location),
                 skip_refresh: Some(skip_refresh),
                 yt_dlp_attempts: Some(yt_dlp_attempts),
             },
-            cli::Commands::Open { url } => Self {
+            cli::Commands::Open {
+                url,
+                cache_dir,
+                newsboat_config_location,
+                cache_db_location,
+                log_level,
+            } => Self {
                 cmd_type: CmdType::Open,
-                log_level: cli.log_level,
-                cache_dir: cli.cache_dir,
-                newsboat_config_location: cli.newsboat_config_location,
-                cache_db_location: cli.cache_db_location,
+                log_level: log_level,
+                cache_dir: cache_dir,
+                newsboat_config_location: newsboat_config_location,
+                cache_db_location: cache_db_location,
                 open_url: Some(url),
                 newsboat_urls_location: None,
                 skip_refresh: None,
                 yt_dlp_attempts: None,
             },
 
-            cli::Commands::Clean {} => Self {
+            cli::Commands::Clean {
+                cache_dir,
+                newsboat_config_location,
+                cache_db_location,
+                log_level,
+            } => Self {
                 cmd_type: CmdType::Clean,
-                log_level: cli.log_level,
-                cache_dir: cli.cache_dir,
-                newsboat_config_location: cli.newsboat_config_location,
-                cache_db_location: cli.cache_db_location,
+                log_level: log_level,
+                cache_dir: cache_dir,
+                newsboat_config_location: newsboat_config_location,
+                cache_db_location: cache_db_location,
                 open_url: None,
                 newsboat_urls_location: None,
                 skip_refresh: None,
                 yt_dlp_attempts: None,
+            },
+
+            cli::Commands::Update {
+                newsboat_urls_location,
+                skip_refresh,
+                yt_dlp_attempts,
+                cache_dir,
+                newsboat_config_location,
+                cache_db_location,
+                log_level,
+            } => Self {
+                cmd_type: CmdType::Dl,
+                log_level: log_level,
+                cache_dir: cache_dir,
+                newsboat_config_location: newsboat_config_location,
+                cache_db_location: cache_db_location,
+                open_url: None,
+                newsboat_urls_location: Some(newsboat_urls_location),
+                skip_refresh: Some(skip_refresh),
+                yt_dlp_attempts: Some(yt_dlp_attempts),
             },
         }
     }
